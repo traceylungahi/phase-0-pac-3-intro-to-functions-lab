fe4db479a31c26a025eeba25f13568d63f7409fc
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-03 10:02:31 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    "Hello!".toUpperCase();
+    return string.toUpperCase();
+}
+function whisper(string) {
+    "Hello!".toLowerCase();
+    return string.toLowerCase();
+}
+function logShout(string) {
+    'hello'.toUpperCase();
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    'HELLO'.toLowerCase();
+    console.log(string.toLowerCase());
+}function sayHiToHeadphonedRoommate(string) {
+    switch (string) {
+        case string.toLowerCase():
+            return "I can\'t hear you!";
+            break;
+        case string.toUpperCase():
+            return "YES INDEED!";
+            break;
+        case string = "Let's have dinner together!":
+            return "I would love to!"
+            break;
+       default:
+           break;
+    }
+}
+    
+    
